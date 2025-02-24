101eed19cf21a014082fd27ff73c465a1db6728c
Remi Vidal <rvidal@aubay.com>
2025-02-24 10:27:37 +0100

    Adding password

--- app.py
+++ app.py
@@ -17,25 +17,29 @@ from utils import getResume, anonymize_cv, extract_text_from_pdf
 # from dotenv import load_dotenv
 # load_dotenv()  ## load all our environment variables
 # genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# PASSWORD = os.getenv("PASSWORD")
+
 
 api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
+PASSWORD = st.secrets["PASSWORD"]
+
 
 
 def get_gemini_response(input_text, max_retries=5, base_wait=30):
     """
     Génère une réponse en gérant les erreurs de quota (429).
-    
+
     Args:
         input_text (str): Texte d'entrée pour le modèle.
         max_retries (int): Nombre maximum de tentatives avant d'abandonner.
         base_wait (int): Temps d'attente initial (en secondes) avant le premier retry.
-    
+
     Returns:
         dict: La réponse du modèle sous forme de JSON.
     """
     model = genai.GenerativeModel("gemini-1.5-flash")
-    
+
     for attempt in range(max_retries):
         try:
             response = model.generate_content(
@@ -53,99 +57,106 @@ def get_gemini_response(input_text, max_retries=5, base_wait=30):
         except Exception as e:
             st.error(f"Erreur inattendue : {e}")
             return {"Année de diplomation": "N/A", "Compétences": "N/A"}  # Valeurs par défaut en cas d'erreur fatale
-    
+
     st.error("Échec après plusieurs tentatives. Veuillez réessayer plus tard.")
     return {"Année de diplomation": "N/A", "Compétences": "N/A"}  # Valeurs par défaut si toutes les tentatives échouent
-
-
-# Prompt Template
-input_prompt = """
-Tu joues le rôle d'un recruteur data qui doit extraire des informations clés d'un CV.
-Un(e) candidat(e) a envoyé son CV par mail.
-
-Retrouve l'année de diplomation et les 5 compétences techniques data clés dans le CV ci-dessous.
-
-Pour l'année, fais attention car parfois une formation est spécifiée avec les dates de début et de fin.
-Par exemple : 09/2022 - 06/2024 ou bien 2021 à 2022. Dans ces cas-là, il faut aller chercher l'année de fin, c'est-à-dire
-respectivement 2024 et 2022.
-
-CV: {text}
-
-Je veux une réponse en un seul string ayant la structure suivante :
-{{"Année de diplomation": "YYYY", "Compétences": "compétence1, compétence2, compétence3, compétence4, compétence5"}}
-"""
 
 ## Streamlit app
 st.set_page_config(layout="wide")  # Wide mode by default
 st.title("ATS")
 
-# Upload files via drag-and-drop
-uploaded_files = st.file_uploader(
-    "Glissez-Déposez vos fichiers .msg ici", 
-    type=["msg"], 
-    accept_multiple_files=True
-)
+if "authenticated" not in st.session_state:
+    st.session_state["authenticated"] = False
 
-if uploaded_files:
+def login():
+    st.title("🔐 Connexion")
+    password_input = st.text_input("Mot de passe :", type="password")
 
-    cvs_folder = "CVs"
-    all_responses = []
-    batch_size = 15  # Nombre max de requêtes par minute
+    if st.button("Se connecter"):
+        if password_input == PASSWORD:
+            st.session_state["authenticated"] = True
+            st.rerun()
+        else:
+            st.error("Mot de passe incorrect.")
 
-    # Get the list of all .msg files
-    total_mails = len(uploaded_files)
-    processed_mails = 0
+if not st.session_state["authenticated"]:
+    login()
+else:
+    st.success("✅ Accès autorisé !")
+    # Ton application ici
 
-    # Initialize progress bar
-    progress_bar = st.progress(0)
-    progress_text = st.empty()
-    pause_text = st.empty()
 
-    # Batch processing .msg files
 
-    for i in range(0, total_mails, batch_size):
-        batch_files = uploaded_files[i:i + batch_size]
+    # Prompt Template
+    input_prompt = """
+    Tu joues le rôle d'un recruteur data qui doit extraire des informations clés d'un CV.
+    Un(e) candidat(e) a envoyé son CV par mail.
 
-        for file in batch_files:
-            filename = file.name
-            print(f"Processing: {filename}")
+    Retrouve l'année de diplomation et les 5 compétences techniques data clés dans le CV ci-dessous.
 
-            # Extraire le job depuis le nom de l'email
-            match = re.search(r'application_ (.*?) from', filename)
-            job_name = match.group(1) if match else "Inconnu"
+    Pour l'année, fais attention car parfois une formation est spécifiée avec les dates de début et de fin.
+    Par exemple : 09/2022 - 06/2024 ou bien 2021 à 2022. Dans ces cas-là, il faut aller chercher l'année de fin, c'est-à-dire
+    respectivement 2024 et 2022.
 
-            # Extract names from the email
-            email_name = filename.split("from")[1].split(".msg")[0] if "from" in filename else "Inconnu"
-            noms_from_email = email_name.split()
-            print("Noms de l'email :", noms_from_email)
+    CV: {text}
 
-            # Lecture du fichier .msg directement depuis l'upload
-            msg_bytes = file.read()
-            msg = extract_msg.Message(io.BytesIO(msg_bytes))
+    Je veux une réponse en un seul string ayant la structure suivante :
+    {{"Année de diplomation": "YYYY", "Compétences": "compétence1, compétence2, compétence3, compétence4, compétence5"}}
+    """
 
-            # Resume extraction
-            final_path = getResume(msg, cvs_folder)
+    
 
-            if not final_path : #Notamment si c'est un word
-                logging.error(f"Skipping email {filename}, no valid CV found.")
-                response_data = {"Année de diplomation": "N/A", "Compétences": "N/A"}
+    # Upload files via drag-and-drop
+    uploaded_files = st.file_uploader(
+        "Glissez-Déposez vos fichiers .msg ici", 
+        type=["msg"], 
+        accept_multiple_files=True
+    )
 
-                all_responses.append(
-                    {
-                        "Job": job_name,
-                        "Mail": "N/A",
-                        "Nom": " ".join(noms_from_email),
-                        "Diplôme": "N/A",
-                        "Compétences Tech": "N/A",
-                    }
-                )
-            else:  # If a file has been successfully saved
-                text_cv = extract_text_from_pdf(final_path)
+    if uploaded_files:
 
-                # Anonymization
-                text_anonymise, extracted_email = anonymize_cv(text_cv, [name for name in noms_from_email if len(name) > 2])
+        cvs_folder = "CVs"
+        all_responses = []
+        batch_size = 15  # Nombre max de requêtes par minute
 
-                if text_anonymise == "":
+        # Get the list of all .msg files
+        total_mails = len(uploaded_files)
+        processed_mails = 0
+
+        # Initialize progress bar
+        progress_bar = st.progress(0)
+        progress_text = st.empty()
+        pause_text = st.empty()
+
+        # Batch processing .msg files
+
+        for i in range(0, total_mails, batch_size):
+            batch_files = uploaded_files[i:i + batch_size]
+
+            for file in batch_files:
+                filename = file.name
+                print(f"Processing: {filename}")
+
+                # Extraire le job depuis le nom de l'email
+                match = re.search(r'application_ (.*?) from', filename)
+                job_name = match.group(1) if match else "Inconnu"
+
+                # Extract names from the email
+                email_name = filename.split("from")[1].split(".msg")[0] if "from" in filename else "Inconnu"
+                noms_from_email = email_name.split()
+                print("Noms de l'email :", noms_from_email)
+
+                # Lecture du fichier .msg directement depuis l'upload
+                msg_bytes = file.read()
+                msg = extract_msg.Message(io.BytesIO(msg_bytes))
+
+                # Resume extraction
+                final_path = getResume(msg, cvs_folder)
+
+                if not final_path : #Notamment si c'est un word
+                    logging.error(f"Skipping email {filename}, no valid CV found.")
+                    response_data = {"Année de diplomation": "N/A", "Compétences": "N/A"}
+
                     all_responses.append(
                         {
                             "Job": job_name,
@@ -155,43 +166,59 @@ if uploaded_files:
                             "Compétences Tech": "N/A",
                         }
                     )
+                else:  # If a file has been successfully saved
+                    text_cv = extract_text_from_pdf(final_path)
 
-                # print("Texte anonymisé :", text_anonymise)
-                # print("Email extrait :", extracted_email)
+                    # Anonymization
+                    text_anonymise, extracted_email = anonymize_cv(text_cv, [name for name in noms_from_email if len(name) > 2])
 
-                else:
-                    # Si le CV a du contenu, on le fournit au LLM
-                    formatted_prompt = input_prompt.format(text=text_anonymise)
-                    response = get_gemini_response(formatted_prompt)
-                    print("Réponse : ", response)
+                    if text_anonymise == "":
+                        all_responses.append(
+                            {
+                                "Job": job_name,
+                                "Mail": "N/A",
+                                "Nom": " ".join(noms_from_email),
+                                "Diplôme": "N/A",
+                                "Compétences Tech": "N/A",
+                            }
+                        )
 
-                    all_responses.append(
-                        {   
-                            "Job": job_name,
-                            "Mail": extracted_email,
-                            "Nom": " ".join(noms_from_email),
-                            "Diplôme": response["Année de diplomation"],
-                            "Compétences Tech": response["Compétences"],
-                        }
-                    )  # On retourne le texte anonymisé + l'email et le nom extraits
+                    # print("Texte anonymisé :", text_anonymise)
+                    # print("Email extrait :", extracted_email)
 
-            # Update progress
-            processed_mails += 1
-            progress_bar.progress(processed_mails / total_mails)
-            progress_text.text(f"{processed_mails} mails traités sur {total_mails}")
+                    else:
+                        # Si le CV a du contenu, on le fournit au LLM
+                        formatted_prompt = input_prompt.format(text=text_anonymise)
+                        response = get_gemini_response(formatted_prompt)
+                        print("Réponse : ", response)
 
-        if i + batch_size < total_mails:
-            pause_text.text("Pause de 1 minute pour respecter le quota de l'API...")
-            time.sleep(60)
-            pause_text.empty()  # Clear the pause message after the pause
+                        all_responses.append(
+                            {   
+                                "Job": job_name,
+                                "Mail": extracted_email,
+                                "Nom": " ".join(noms_from_email),
+                                "Diplôme": response["Année de diplomation"],
+                                "Compétences Tech": response["Compétences"],
+                            }
+                        )  # On retourne le texte anonymisé + l'email et le nom extraits
 
-    # Create a DataFrame
-    df = pd.DataFrame(all_responses)
-    df = df.sort_values(by="Job").reset_index(drop=True)
-    # Display the DataFrame as a table
-    st.dataframe(df)
+                # Update progress
+                processed_mails += 1
+                progress_bar.progress(processed_mails / total_mails)
+                progress_text.text(f"{processed_mails} mails traités sur {total_mails}")
+
+            if i + batch_size < total_mails:
+                pause_text.text("Pause de 1 minute pour respecter le quota de l'API...")
+                time.sleep(60)
+                pause_text.empty()  # Clear the pause message after the pause
+
+        # Create a DataFrame
+        df = pd.DataFrame(all_responses)
+        df = df.sort_values(by="Job").reset_index(drop=True)
+        # Display the DataFrame as a table
+        st.dataframe(df)
 
 
-    if os.path.exists(cvs_folder):
-        shutil.rmtree(cvs_folder)  # Remove folder and its content
-        os.makedirs(cvs_folder)  # Recreate folder if another script needs it
+        if os.path.exists(cvs_folder):
+            shutil.rmtree(cvs_folder)  # Remove folder and its content
+            os.makedirs(cvs_folder)  # Recreate folder if another script needs it
