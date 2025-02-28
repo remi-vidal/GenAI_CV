86260d5a0f1c95f379e9239d8a1c313501926ead
Remi Vidal <rvidal@aubay.com>
2025-02-28 01:36:39 +0100

    v0.2.2

--- upload.py
+++ upload.py
@@ -14,20 +14,20 @@ from google.generativeai.types import GenerationConfig
 from pymongo import MongoClient
 from utils import *
 
-from dotenv import load_dotenv
-load_dotenv()  ## load all our environment variables
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["staging"]
-collection = db["data_test"]
-
-# api_key = st.secrets["GOOGLE_API_KEY"]
-# genai.configure(api_key=api_key)
-# MONGO_URI = st.secrets["MONGO_URI"]
+# from dotenv import load_dotenv
+# load_dotenv()  ## load all our environment variables
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# MONGO_URI = os.getenv("MONGO_URI")
 # client = MongoClient(MONGO_URI)
-# db = client["ats_database"]
-# collection = db["candidatures"]
+# db = client["staging"]
+# collection = db["data_test"]
+
+api_key = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=api_key)
+MONGO_URI = st.secrets["MONGO_URI"]
+client = MongoClient(MONGO_URI)
+db = client["ats_database"]
+collection = db["candidatures"]
 
 def insert_into_mongo(data):
     """
