4bd6043e7e20cc829f081f5e518de52aefd3b2cc
Remi Vidal <rvidal@aubay.com>
2025-03-05 00:15:59 +0100

    add cache + put nav buttons before data editor

--- gestion.py
+++ gestion.py
@@ -3,6 +3,12 @@ import streamlit as st
 import pandas as pd
 from config import collection
 
+
+@st.cache_data(ttl=60)  # Cache pendant 60s
+def load_data(skip, limit, filters, sort_columns, sort_orders):
+    return get_applications(skip, limit, filters, sort_columns, sort_orders)
+
+
 def get_applications(skip=0, limit=20, filters=None, sort_columns=None, sort_orders=None):
     """
     Retrieve applications from the MongoDB database with pagination, filters, and sorting.
@@ -93,7 +99,35 @@ def gestion_page():
     sort_columns = st.multiselect("↕️ Trier par :", collection.find_one().keys(), on_change=lambda: st.session_state.update(page=0))
     sort_orders = [st.checkbox(f"Ordre croissant pour {col}", value=True, on_change=lambda: st.session_state.update(page=0)) for col in sort_columns]
 
-    df = get_applications(skip=skip, limit=page_size, filters=filters, sort_columns=sort_columns, sort_orders=sort_orders)
+    df = load_data(skip, page_size, filters, sort_columns, sort_orders)
+
+    col_pag1, col_pag2, col_pag3, col_pag4, col_pag5, col_pag6 = st.columns([13, 2, 1, 1, 1, 1])  # Ajuste la répartition des colonnes
+
+
+
+    with col_pag3:
+        if st.button("⏮️"):  # Aller à la première page
+            st.session_state.page = 0
+            st.rerun()
+
+    with col_pag4:
+        if st.button("⬅️") and st.session_state.page > 0:
+            st.session_state.page -= 1
+            st.rerun()
+
+    
+    with col_pag5:
+        if st.button("➡️") and len(df) == page_size:
+            st.session_state.page += 1
+            st.rerun()
+
+    with col_pag6:
+        total_docs = collection.count_documents(filters)  # Compte total des documents selon les filtres
+        last_page = (total_docs // page_size) if total_docs % page_size == 0 else (total_docs // page_size)
+        
+        if st.button("⏭️"):  # Aller à la dernière page
+            st.session_state.page = last_page
+            st.rerun()
 
     if not df.empty:
 
@@ -101,35 +135,9 @@ def gestion_page():
         total_pages = (total_docs // page_size) + (1 if total_docs % page_size != 0 else 0)
         current_page = st.session_state.page + 1
 
-        col_pag1, col_pag2, col_pag3, col_pag4, col_pag5, col_pag6 = st.columns([13, 2, 1, 1, 1, 1])  # Ajuste la répartition des colonnes
-
-
         with col_pag2:
             st.write(f"Page {current_page}/{total_pages}")
 
-        with col_pag3:
-            if st.button("⏮️"):  # Aller à la première page
-                st.session_state.page = 0
-                st.rerun()
-
-        with col_pag4:
-            if st.button("⬅️") and st.session_state.page > 0:
-                st.session_state.page -= 1
-                st.rerun()
-
-        
-        with col_pag5:
-            if st.button("➡️") and len(df) == page_size:
-                st.session_state.page += 1
-                st.rerun()
-
-        with col_pag6:
-            total_docs = collection.count_documents(filters)  # Compte total des documents selon les filtres
-            last_page = (total_docs // page_size) if total_docs % page_size == 0 else (total_docs // page_size)
-            
-            if st.button("⏭️"):  # Aller à la dernière page
-                st.session_state.page = last_page
-                st.rerun()
 
         df["Statut"] = df["Statut"].map(STATUT_MAPPING)
 
