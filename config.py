006b50984199330a91c32cc0b4eda86530b2ee1e
Remi Vidal <rvidal@aubay.com>
2025-03-05 00:22:31 +0100

    v0.2.5

--- config.py
+++ config.py
@@ -3,22 +3,21 @@ import google.generativeai as genai
 
 # Staging
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["staging"]
-collection = db["data_test"]
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# MONGO_URI = os.getenv("MONGO_URI")
+# client = MongoClient(MONGO_URI)
+# db = client["staging"]
+# collection = db["data_test"]
 
 # Production
 
-# import streamlit as st
-# PASSWORD = st.secrets["PASSWORD"]
-# api_key = st.secrets["GOOGLE_API_KEY"]
-# genai.configure(api_key=api_key)
-# MONGO_URI = st.secrets["MONGO_URI"]
-# client = MongoClient(MONGO_URI)
-# db = client["ats_database"]
-# collection = db["candidatures"]
+import streamlit as st
+api_key = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=api_key)
+MONGO_URI = st.secrets["MONGO_URI"]
+client = MongoClient(MONGO_URI)
+db = client["ats_database"]
+collection = db["candidatures"]
