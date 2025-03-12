d498094243b603fceb89ad75ead6806410555246
Remi Vidal <rvidal@aubay.com>
2025-03-12 18:39:07 +0100

    change db & collection name for VM

--- config.py
+++ config.py
@@ -7,10 +7,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-MONGO_URI = os.getenv("MONGO_URI")
+# MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = "mongodb://localhost:27017/"
 client = MongoClient(MONGO_URI)
-db = client["staging"]
-collection = db["data_test"]
+# db = client["staging"]
+# collection = db["data_test"]
+
+db = client["ats"]
+collection = db["candidatures"]
 
 # Production
 
